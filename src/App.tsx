20fbb56c9d74c2a157b64371f0d95ae113c6aa68
Rick Brown <ricky.brown.00@gmail.com>
2022-10-26 13:39:52 +0100

    start over

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
-import { memo, useState } from 'react'
-import styles from './App.module.css'
-import Button from './components/Button/Button'
-import PopTart from './components/Button/PopTart/PopTart'
-
-export type _PopTart = {
-	id: number
-	title: string
-	description: string
-	buttonText: string
-	backgroundColor: string
-}
-
 function App() {
 	return (
 		<div>
-			<Toast />
+			<h1>App</h1>
 		</div>
 	)
 }
